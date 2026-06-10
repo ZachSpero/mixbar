0f47e393316e7211ffc06f5530f51e258493c9b0
Zach Spero <zach@realtimemg.com>
2026-06-10 14:43:24 -0400

    Remove tracked .build artifacts (now gitignored)

--- .build/arm64-apple-macosx/debug/MixBarEngine.build/vendor/PublicUtility/BGMDebugLogging.c.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/Darwin.modulemap \
-  /Users/zacharyspero/Code/mixbar/Sources/MixBarEngine/vendor/PublicUtility/BGMDebugLogging.c \
-  /Users/zacharyspero/Code/mixbar/Sources/MixBarEngine/vendor/PublicUtility/BGMDebugLogging.h
